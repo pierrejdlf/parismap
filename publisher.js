cfbfb0fb3432338ceabfbde633bb204506683c15
stephanebachelier <stephane.bachelier@gmail.com>
2013-06-08 00:13:09 +0200

    cleaning comment

--- publisher.js
+++ publisher.js
@@ -11,36 +11,3 @@ if (params.ESHQ_KEY) {
 }
 
 module.exports = publisherClient;
-
-
-
-
-/*
-var redis = require('redis');
-var params = require('./myparams.js');
-
-if(params.where=="local") {
-	publisherClient = redis.createClient();
-}
-else if(params.where=="jitsu") {
-	publisherClient = redis.createClient(6379, 'nodejitsudb9770346188.redis.irstack.com');
-	publisherClient.auth('nodejitsudb9770346188.redis.irstack.com:f327cfe980c971946e80b8e975fbebb4', function (err) {
-		if (err) { throw err; }
-	});
-}
-else if(params.where=="heroku") {
-	if (process.env.REDISTOGO_URL) {
-		var rtg = require("url").parse(process.env.REDISTOGO_URL);
-		publisherClient = redis.createClient(rtg.port, rtg.hostname);
-		publisherClient.auth(rtg.auth.split(":")[1], function (err) {
-			if (err) { throw err; }
-		}); 
-	} else {
-		console.log("ERROR: no process.env.REDISTOGO_URL");
-	}
-}
-
-module.exports = publisherClient;
-*/
-
-
